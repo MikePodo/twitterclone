c7fd8a00029deb806ae2904a796a6f70f3c2c5e9
MikePodo <72575260+MikePodo@users.noreply.github.com>
2020-11-30 17:11:51 -0800

    responsive

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,7 +1,11 @@
 import React from "react";
-//Fontawesome
-
+//Icons
 import { FaTwitter } from "react-icons/fa";
+import { RiHome7Fill, RiFileList2Line } from "react-icons/ri";
+import { BiHash, BiBell } from "react-icons/bi";
+import { FiMail } from "react-icons/fi";
+import { BsBookmark } from "react-icons/bs";
+import { CgProfile, CgMoreO } from "react-icons/cg";
 const Left = ({ leftStatus, setLeftStatus }) => {
   return (
     <div
@@ -20,27 +24,35 @@ const Left = ({ leftStatus, setLeftStatus }) => {
             <div className="line2"></div>
           </div>
           <div className="option">
+            <RiHome7Fill className="option-icon" />
             <h2>Home</h2>
           </div>
           <div className="option">
+            <BiHash className="option-icon" />
             <h2>Explore</h2>
           </div>
           <div className="option">
+            <BiBell className="option-icon" />
             <h2>Notifications</h2>
           </div>
           <div className="option">
+            <FiMail className="option-icon" />
             <h2>Messages</h2>
           </div>
           <div className="option">
+            <BsBookmark className="option-icon" />
             <h2>Bookmarks</h2>
           </div>
           <div className="option">
+            <RiFileList2Line className="option-icon" />
             <h2>Lists</h2>
           </div>
           <div className="option">
+            <CgProfile className="option-icon" />
             <h2>Profile</h2>
           </div>
           <div className="option">
+            <CgMoreO className="option-icon" />
             <h2>More</h2>
           </div>
           <button className="twitter-button">Tweet</button>
